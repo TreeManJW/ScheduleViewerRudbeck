22206f9540bc258209b2d93549b4fd31d4f12158
Jonatan Wåger <jowag_06@edu.sollentuna.se>
2024-06-15 13:49:56 +0200

    Cleanup

--- app.py
+++ app.py
@@ -3,6 +3,21 @@ import streamlit as st
 col = 5
 row = 6
 
+columns = st.columns(col)
+sidebar = st.sidebar
+
+columnHeaders = ["Mån", "Tis", "Ons", "Tors", "Fre"]
+
+incrementPixels = 10
+start_hour = 8
+start_minute = 20
+end_hour = 16
+end_minute = 5
+
+time_bar_html = """
+<div style="height: 100%; display: flex; flex-direction: column; justify-content: space-between; margin-right: 10px;">
+"""
+
 block1 = True
 block2 = True
 block3 = True
@@ -14,7 +29,31 @@ block8 = True
 ment1 = True
 ment2 = True
 
-sidebar = st.sidebar
+boxWidth = 130
+boxHeights = [
+    [200, 200, 200, 200, 200],
+    [100, 100, 100, 100, 100],
+    [100, 50, 100, 100, 90],
+    [100, 50, 100, 100, 90],
+    [100, 100, 100, 100, 100],
+    [100, 200, 100, 100, 100]
+]
+boxNames = [
+    ["1", "5", "2", "7", "6"],
+    ["2", "6", "1", "8", "5"],
+    ["3", "M1", "4", "3", "4"],
+    ["4", "M2", "3", "4", "3"],
+    ["5", "7", "", "1", "8"],
+    ["6", "8", "", "2", "7"]
+]
+showBlocks = [
+    [block1, block5, block2, block7, block6],
+    [block2, block6, block1, block8, block5],
+    [block3, ment1, block4, block3, block4],
+    [block4, ment2, block3, block4, block3],
+    [block5, block7, False, block1, block8],
+    [block6, block8, False, block2, block7]
+]
 
 block1 = sidebar.checkbox("Block 1", value=block1)
 block2 = sidebar.checkbox("Block 2", value=block2)
@@ -27,49 +66,8 @@ block8 = sidebar.checkbox("Block 8", value=block8)
 ment1 = sidebar.checkbox("Ment 1", value=ment1)
 ment2 = sidebar.checkbox("Ment 2", value=ment2)
 
-columnHeaders = ["Mån", "Tis", "Ons", "Tors", "Fre"]
-columns = st.columns(col)
 for i in range(col):
     columns[i].write(f"<div style='text-align: center;'>{columnHeaders[i]}</div>", unsafe_allow_html=True)
-
-boxWidth = 130
-boxHeights = [
-    [200, 200, 200, 200, 200],
-    [100, 100, 100, 100, 100],
-    [100, 50, 100, 100, 90],
-    [100, 50, 100, 100, 90],
-    [100, 100, 100, 100, 100],
-    [100, 200, 100, 100, 100]
-]
-
-boxNames = [
-    ["1", "5", "2", "7", "6"],
-    ["2", "6", "1", "8", "5"],
-    ["3", "M1", "4", "3", "4"],
-    ["4", "M2", "3", "4", "3"],
-    ["5", "7", "", "1", "8"],
-    ["6", "8", "", "2", "7"]
-]
-
-showBlocks = [
-    [block1, block5, block2, block7, block6],
-    [block2, block6, block1, block8, block5],
-    [block3, ment1, block4, block3, block4],
-    [block4, ment2, block3, block4, block3],
-    [block5, block7, False, block1, block8],
-    [block6, block8, False, block2, block7]
-]
-
-incrementPixels = 10
-
-time_bar_html = """
-<div style="height: 100%; display: flex; flex-direction: column; justify-content: space-between; margin-right: 10px;">
-"""
-
-start_hour = 8
-start_minute = 20
-end_hour = 16
-end_minute = 5
 
 current_hour = start_hour
 current_minute = start_minute
@@ -96,7 +94,7 @@ for j in range(row):
             """
         else:
             boxHtml = f"""
-            <div style="width: {boxWidth}px; height: {boxHeights[j][i]}px; border: 1px solid black; padding: 10px; text-align: center;">
+            <div style="width: {boxWidth}px; height: {boxHeights[j][i]}px; border: 1px solid black; padding: 10px; opacity: 0.1; text-align: center;">
             </div>
             """
         block_row[i].markdown(boxHtml, unsafe_allow_html=True)
