5277fddec49ce09e42a3617242eb222e66dcb7ed
Jonatan Wåger <jowag_06@edu.sollentuna.se>
2024-08-12 12:52:44 +0200

    Returned to original python app because it works and I need this done soon. Note, this version has a flaw I'm trying to fix so not technically done but close enough!

--- app.py
+++ app.py
@@ -9,17 +9,6 @@ sidebar = st.sidebar
 
 columnHeaders = ["Mån", "Tis", "Ons", "Tors", "Fre"]
 
-# Variables to create lines with 5 minute, 10 pixels increments that range from 8:20 to 16:05. The longest possible day.
-incrementPixels = 10
-start_hour = 8
-start_minute = 20
-end_hour = 16
-end_minute = 5
-
-time_bar_html = """
-<div style="height: 100%; display: flex; flex-direction: column; justify-content: space-between; margin-right: 10px;">
-"""
-
 # Variables that you can change with the sidebar and that are used to check if the block should be vissible.
 block1 = True
 block2 = True
@@ -50,14 +39,7 @@ boxNames = [
     ["5", "7", "", "1", "8"],
     ["6", "8", "", "2", "7"]
 ]
-showBlocks = [
-    [block1, block5, block2, block7, block6],
-    [block2, block6, block1, block8, block5],
-    [block3, ment1, block4, block3, block4],
-    [block4, ment2, block3, block4, block3],
-    [block5, block7, False, block1, block8],
-    [block6, block8, False, block2, block7]
-]
+
 
 #Setting up the sidebar.
 block1 = sidebar.checkbox("Block 1", value=block1)
@@ -71,24 +53,18 @@ block8 = sidebar.checkbox("Block 8", value=block8)
 ment1 = sidebar.checkbox("Ment 1", value=ment1)
 ment2 = sidebar.checkbox("Ment 2", value=ment2)
 
+showBlocks = [
+    [block1, block5, block2, block7, block6],
+    [block2, block6, block1, block8, block5],
+    [block3, ment1, block4, block3, block4],
+    [block4, ment2, block3, block4, block3],
+    [block5, block7, False, block1, block8],
+    [block6, block8, False, block2, block7]
+]
+
 #Writing the day heathers.
 for i in range(col):
     columns[i].write(f"<div style='text-align: center;'>{columnHeaders[i]}</div>", unsafe_allow_html=True)
-
-# Shit I will fix later.
-current_hour = start_hour
-current_minute = start_minute
-
-while current_hour < end_hour or (current_hour == end_hour and current_minute <= end_minute):
-    time_bar_html += f"""<div style="height: 1px; background-color: #ccc; margin-bottom: {incrementPixels}px; opacity: 0.1;"></div>"""
-
-    current_minute += 5
-    if current_minute >= 60:
-        current_minute -= 60
-        current_hour += 1
-
-time_bar_html += "</div>"
-st.markdown(time_bar_html, unsafe_allow_html=True)
 
 # The actuall code to show all the boxes. It itterates through all boxes and checks if it should be vissible.
 for j in range(row):
@@ -102,9 +78,12 @@ for j in range(row):
             </div>
             """
         #If the box is hidden it should create an invissible or in this case barely vissible outline of where that block. This ensures the other boxes are at the same place all the time.
+        elif boxNames[j][i] == "M1" or boxNames[j][i] == "M2" and showBlocks[j][i] == False:
+            boxHtml = ""
+            #print("removed block " + boxNames[j][i])
         else:
             boxHtml = f"""
-            <div style="width: {boxWidth}px; height: {boxHeights[j][i]}px; border: 1px solid black; padding: 10px; opacity: 0.1; text-align: center;">
+            <div style="width: {boxWidth}px; height: {boxHeights[j][i]}px; border: 1px solid black; padding: 10px; opacity: 0,1; text-align: center;">
             </div>
             """
         block_row[i].markdown(boxHtml, unsafe_allow_html=True)
--- app2.py
+++ app2.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-# 5 columns for all days on a school week. 93 rows, one for each minute on a school day.
+# 5 columns for all days on a school week. 93 rows, one for each five minute interval on a school day.
 col = 5
 row = 93
 
@@ -16,3 +16,21 @@ time_bar_html = """
 #Writing the day heathers.
 for i in range(col):
     columns[i].write(f"<div style='text-align: center;'>{columnHeaders[i]}</div>", unsafe_allow_html=True)
+
+# The actuall code to show all the boxes. It itterates through all boxes and checks if it should be vissible.
+for j in range(row):
+    block_row = st.columns(col)
+    for i in range(col):
+        #If the box is vissible we create it using HTML an the previously set variables on heights and names.
+        #if showBlocks[j][i]:
+        boxHtml = f"""
+        <div style="width: 130px; height: 2px; border: 0px solid black; background-color: lightgrey; padding: 0px; text-align: center;">
+        </div>
+        """
+        #If the box is hidden it should create an invissible or in this case barely vissible outline of where that block. This ensures the other boxes are at the same place all the time.
+        #else:
+        #    boxHtml = f"""
+        #    <div style="width: {boxWidth}px; height: {boxHeights[j][i]}px; border: 1px solid black; padding: 10px; opacity: 0.1; text-align: center;">
+        #    </div>
+        #    """
+        block_row[i].markdown(boxHtml, unsafe_allow_html=True)
